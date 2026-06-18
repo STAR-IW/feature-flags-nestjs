22b68730d8c1372255e73a54b7737fab85ea6f5f
Itai <starnes.i@gmail.com>
2026-06-18 17:19:24 +0300

    add launchdarkly feature flag integration

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, OnModuleDestroy, OnModuleInit} from '@nestjs/common';
+import {ConfigService} from "@nestjs/config";
+import * as LaunchDarkly from '@launchdarkly/node-server-sdk';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AppService implements OnModuleInit, OnModuleDestroy {
+  private ldClient : LaunchDarkly.LDClient;
+  constructor(private configService: ConfigService) {
+  }
+  async onModuleInit(){
+    const sdk = this.configService.get<string>('LAUNCHDARKLY_SDK_KEY');
+    this.ldClient = LaunchDarkly.init(sdk!);
+    await this.ldClient.waitForInitialization()
+    console.log('SDK successfully initialized!')
+  }
+  onModuleDestroy(): any {
+    this.ldClient.close();
+  }
+
+  async getTime(): Promise<string> {
+    const flagName = 'time_feature'
+    // const context = {key:'custom_user'}
+    const context = {key:'usa'}
+    const isFeatureFlag =  await this.ldClient.variation(flagName, context ,false )
+    if(isFeatureFlag){
+      return 'Good Evening!🌙'
+    }
+    return 'Good Morning!☀️';
   }
 }
